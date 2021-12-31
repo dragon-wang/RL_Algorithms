2b6e00581c981243ac9f069cac6f64f73ce5d294
wangtao <865471687@qq.com>
2021-12-31 12:50:26 +0800

    Add Offline to Online buffer

--- common/buffers.py
+++ common/buffers.py
@@ -90,3 +90,40 @@ class OfflineBufferAtari:
                     rews=torch.FloatTensor(self.rews[ind]),  # 1D
                     next_obs=torch.FloatTensor(next_obs),
                     done=torch.FloatTensor(self.done[ind]))  # 1D
+
+
+class OfflineToOnlineBuffer:
+    """
+    Used in offline to Online setting
+    """
+    def __init__(self, data: dict, batch_size: int):
+        self.obs = data["obs"]
+        self.acts = data["acts"]
+        self.rews = data["rews"]
+        self.next_obs = data["next_obs"]
+        self.done = data["done"]
+
+        self.data_num = self.acts.shape[0]
+        self.batch_size = batch_size
+        self.max_size = self.data_num
+
+        self.ptr = 0  # Point to the current position in the buffer
+        self.crt_size = 0  # The current size of the buffer
+
+    def add(self, obs, act, rew, next_obs, done):
+        self.obs[self.ptr] = obs
+        self.acts[self.ptr] = act
+        self.rews[self.ptr] = rew
+        self.next_obs[self.ptr] = next_obs
+        self.done[self.ptr] = float(done)
+
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.crt_size = min(self.crt_size + 1, self.max_size)
+
+    def sample(self) -> dict:
+        ind = np.random.choice(self.data_num, size=self.batch_size, replace=True)  # replace=False will make sample be slow
+        return dict(obs=torch.FloatTensor(self.obs[ind]),
+                    acts=torch.FloatTensor(self.acts[ind]),
+                    rews=torch.FloatTensor(self.rews[ind]),  # 1D
+                    next_obs=torch.FloatTensor(self.next_obs[ind]),
+                    done=torch.FloatTensor(self.done[ind]))  # 1D
